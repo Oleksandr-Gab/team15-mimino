4d86585ad79bb0fc20cdb7da52874e640efa0923
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-03-02 14:48:40 +0200

    проверка

--- src/main.js
+++ src/main.js
@@ -7,3 +7,19 @@ import './js/modal-room';
 import './js/modal-table-hero';
 import './js/modal-table-page-rest-menu';
 import './js/modal-price-page-hotel-hero';
+import './js/modal-page-hotel-price-four';
+import './js/modal-page-hotel-price-lux';
+import './js/modal-page-hotel-price-tri';
+import './js/modal-page-room';
+import './js/modal-price-page-hotel-hero';
+import './js/modal-room-hot';
+import './js/modal-room-page-hot';
+import './js/modal-room-page-room';
+import './js/modal-room-room-am';
+import './js/modal-table-page-rest-menu-slick-open';
+import './js/modal-table-page-rest-menu';
+import './js/modal-table-restoran';
+import './js/modal-table';
+
+
+
